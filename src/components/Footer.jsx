89f25f0e534b361e52e05c8ac575490afbe576ac
Raptor <francyoppureno@gmail.com>
2023-10-31 13:48:18 +0100

    Gestione componenti
    Spostato tutto nella cartella components, aggiunto Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col justify-center items-center bg-[#121212] h-fit p-4 text-white'>
+      <div>
+        <p>2023 Diritti riservati</p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
