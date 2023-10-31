67f3a5c2fbe9a9a0924361f9e296ceb755746519
Raptor <francyoppureno@gmail.com>
2023-10-31 10:37:28 +0100

    Major improvements
    Added remove buttons, grouped multiple items in order list, improved style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { FaCirclePlus } from 'react-icons/fa6';
+import { FaSquarePlus } from 'react-icons/fa6';
+import { FaSquareMinus } from 'react-icons/fa6';
 
 function App() {
   const bar_items = [
@@ -95,47 +96,125 @@ function App() {
     }
   ];
 
-  const [order, setOrder] = useState([]); // Stato per gestire l'ordine dell'utente
+  const [order, setOrder] = useState([]); // State to manage the user's order
 
-  // Funzione per aggiungere un elemento all'ordine
+  // Function to add an item to the order
   const addItemToOrder = (item) => {
-    setOrder([...order, item]);
+    const existingItem = order.find((orderItem) => orderItem.id === item.id);
+    if (existingItem) {
+      // If the item already exists, increase its quantity
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
+    } else {
+      // If the item is not in the order, add it with a quantity of 1
+      setOrder([...order, { ...item, quantity: 1 }]);
+    }
   };
 
-  // Funzione per calcolare il totale dell'ordine
+  // Function to remove an item from the order
+  const removeItemFromOrder = (itemToRemove) => {
+    if (itemToRemove.quantity === 1) {
+      // If the item's quantity is 1, remove it from the order
+      setOrder(order.filter((item) => item.id !== itemToRemove.id));
+    } else {
+      // If the item's quantity is more than 1, decrease its quantity by 1
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === itemToRemove.id
+          ? { ...orderItem, quantity: orderItem.quantity - 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
+    }
+  };
+
+  const clearOrder = () => {
+    setOrder([]);
+  };
+
+  // Function to calculate the total of the order
   const calculateTotal = () => {
-    return order.reduce((total, item) => total + item.price, 0);
+    return order.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
   return (
-    <div className="bg-[#121212] min-h-screen text-white p-12">
-      <h1 className="text-4xl font-bold mb-4">School Bar Price Calculator</h1>
+    <div className="bg-[#121212] min-h-screen text-white py-12 px-4">
+      <h1 className="text-4xl font-bold mb-8 text-center">School Bar Price Calculator</h1>
 
       <div className="rounded-md bg-[#303030] shadow-md p-4">
         <div className="grid md:grid-cols-2 gap-4">
           <div>
-            <h2 className="text-xl font-semibold">Menu</h2>
+            <h2 className="text-2xl text-center font-semibold mb-3">Menu</h2>
             <ul>
               {bar_items.map((item) => (
-                <li key={item.id}>
-                  {item.name} - ${item.price.toFixed(2)}
-                  <button className='pl-2 h-6' onClick={() => addItemToOrder(item)}> <FaCirclePlus /> </button>
+                <li className="my-2 flex justify-between rounded-md border-solid border-2" key={item.id}>
+                  <div>
+                    <p className="p-2">
+                      {item.name}
+                    </p>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div className='mr-1'>
+                      <p className="p-2">
+                        €{item.price.toFixed(2)}
+                      </p>
+                    </div>
+                    <div>
+                      <button
+                        className="h-10 w-10 flex items-center justify-center"
+                        onClick={() => addItemToOrder(item)}
+                      >
+                        <FaSquarePlus className="text-3xl" />
+                      </button>
+                    </div>
+                  </div>
                 </li>
               ))}
             </ul>
           </div>
 
-          <div>
-            <h2 className="text-xl font-semibold">Order</h2>
+          <div className="border-solid border-t-2 border-neutral-600">
+            <div className="flex justify-between items-center mt-4">
+              <h2 className="text-xl font-semibold">Order</h2>
+              <button
+                className="border-solid border-2 rounded-md px-2 py-1"
+                onClick={() => clearOrder()}
+              >
+                Clear
+              </button>
+            </div>
             <ul>
               {order.map((item) => (
-                <li key={item.id}>
-                  {item.name} - ${item.price.toFixed(2)}
+                <li className="my-2 flex justify-between rounded-md border-solid border-2" key={item.id}>
+                  <div>
+                    <p className="p-2">
+                      {item.quantity > 1 && `x${item.quantity} `}
+                      {item.name}
+                    </p>
+                  </div>
+                  <div className='flex justify-between'>
+                    <div className='mr-1'>
+                      <p className="p-2">
+                        €{item.price.toFixed(2)}
+                      </p>
+                    </div>
+                    <div>
+                      <button
+                        className="h-10 w-10 flex items-center justify-center"
+                        onClick={() => removeItemFromOrder(item)}
+                      >
+                        <FaSquareMinus className="text-3xl" />
+                      </button>
+                    </div>
+                  </div>
                 </li>
               ))}
             </ul>
             <p className="text-xl font-semibold mt-4">
-              Total: ${calculateTotal().toFixed(2)}
+              Total: €{calculateTotal().toFixed(2)}
             </p>
           </div>
         </div>
