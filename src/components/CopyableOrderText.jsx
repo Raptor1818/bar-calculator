8af5e1b9180d97c85f62e8012e608f36a340bcb6
OverKnown1 <francyoppureno@gmail.com>
2023-11-02 17:36:37 +0100

    Aggiunto "Copiato!"
    Aggiunta una span che indica quando la copia e' stata effettuata. Modificato il margin del focus di bottoni e <a> (da focus ad active)

--- src/components/CopyableOrderText.jsx
+++ src/components/CopyableOrderText.jsx
@@ -1,23 +1,38 @@
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaCopy } from "react-icons/fa";
 
 function CopyableOrderText({ order }) {
-  const getOrderText = () => { //crea il testo dell'ordine
-    return order
-      .map((item) => `x${item.quantity} ${item.name}`)
-      .join(",\n");
+  const [copied, setCopied] = useState(false);
+
+  const getOrderText = () => {
+    return order.map((item) => `x${item.quantity} ${item.name}`).join(",\n");
   };
 
   const copyToClipboard = () => {
     const orderText = getOrderText();
     navigator.clipboard.writeText(orderText);
+
+    setCopied(true);
+
+    setTimeout(() => setCopied(false), 3000);
   };
 
-  return ( 
+  useEffect(() => {
+    if (copied) {
+      const timeoutId = setTimeout(() => setCopied(false), 2000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [copied]);
+
+  return (
     <div className="my-4 w-full max-w-3xl">
-      <div className="flex flex-row justify-between border-solid border-[1px] shadow-md shadow-black border-neutral-400 rounded-md mt-2 bg-black font-roboto-mono">
+      <div className="flex flex-row justify-between">
+        <div></div>
+        <div className="relative">
+          <span className={`text-neutral-200 bg-neutral-900 z-10 border-[1px] border-neutral-600 rounded-md p-1 m-2 copied-span ${copied ? "visible" : ""}`}>Copiato!</span>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between border-solid border-[1px] shadow-md shadow-black border-neutral-400 rounded-md bg-black font-roboto-mono">
         <div className="w-full p-4">
           <pre>{getOrderText()}</pre>
         </div>
@@ -25,13 +40,18 @@ function CopyableOrderText({ order }) {
           <button
             className="bg-transparent text-white font-semibold px-2 py-1 shadow-md"
             onClick={copyToClipboard}
-            title="Copia" role="button" aria-label="Copia" type="button"
+            title="Copia"
+            role="button"
+            aria-label="Copia"
+            type="button"
           >
             <FaCopy className="text-lg" title="Copia" role="button" aria-label="Copia" />
           </button>
         </div>
       </div>
-      <p className="text-md text-neutral-100 text-left mt-4">Copia e manda nel gruppo di Whatsapp! Ricorda di aggiungere la classe e l&#39; ora dell&#39; ordine</p>
+      <p className="text-md text-neutral-100 text-center mt-4">
+        Copia e manda nel gruppo di Whatsapp! Ricorda di aggiungere la classe e l' ora dell' ordine
+      </p>
     </div>
   );
 }
