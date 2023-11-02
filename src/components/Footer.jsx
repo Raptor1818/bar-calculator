e861ae2778c5f0eadee4faa9a95eb20c8498f8f8
OverKnown1 <francyoppureno@gmail.com>
2023-11-02 14:47:10 +0100

    Update Footer.jsx
    modificato width per l'estetica del border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,28 @@ import GithubIcon from '/img/github.webp'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-2 bg-[#121212] h-fit p-4 text-neutral-500 text-sm border-t-[1px] border-[#202020] font-roboto-mono'>
-      <div>
-        <p>© 2023 | Tutti i diritti riservati</p>
-      </div>
-      <div>
-      <p>
-        Ideato e sviluppato da {" "}
-        <a
-          href="https://raptorino.com"
-          className="w-fit h-fit relative bg-gradient-to-r from-amber-800 to-yellow-500 bg-clip-text text-transparent"
-          target="_blank"
-        >
-          Francesco Nisi
-          <span className="absolute inset-x-0 bottom-0 w-full h-[2px] bg-gradient-to-r from-amber-800 to-yellow-500"></span>
-        </a>
-      </p>
+    <div className='bg-[#121212] h-fit text-neutral-500 text-sm font-roboto-mono items-center justify-center flex flex-col'>
+      <div className='border-t-[1px] border-[#202020] w-[95%] flex flex-col justify-center items-center gap-2 p-4'>
+        <div>
+          <p>© 2023 | Tutti i diritti riservati</p>
+        </div>
+        <div>
+        <p>
+          Ideato e sviluppato da {" "}
+          <a
+            href="https://raptorino.com"
+            className="w-fit h-fit relative bg-gradient-to-r from-amber-800 to-yellow-500 bg-clip-text text-transparent"
+            target="_blank"
+          >
+            Francesco Nisi
+            <span className="absolute inset-x-0 bottom-0 w-full h-[2px] bg-gradient-to-r from-amber-800 to-yellow-500"></span>
+          </a>
+        </p>
 
-      </div>
-      <div className='mt-2'>
-        <a href="https://github.com/Raptor1818/bar-calculator" target="_blank"><img src={GithubIcon} alt="Github" height={48} width={48} /></a>
+        </div>
+        <div className='mt-2'>
+          <a href="https://github.com/Raptor1818/bar-calculator" target="_blank"><img src={GithubIcon} alt="Github" height={48} width={48} /></a>
+        </div>
       </div>
     </div>
   )
